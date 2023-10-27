fb76f0f30b7d6b738c2b560770dacd80ce0745fd
M-Asimm <kpkprince55persia@gmail.com>
2023-10-27 11:53:14 +0500

    inline styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,3 +7,30 @@ function App() {
 }
 
 export default App;
+
+/**
+ * Inline stylings
+ * ******************************************************
+ */
+
+// const style1 = {
+//   backgroundColor: "red",
+//   color: "white",
+// };
+
+// const style2 = {
+//   fontSize: "30px",
+//   textDecoration: "underline",
+// };
+
+// function App() {
+//   return (
+//     <>
+//       <h1 style={{ padding: "0px", margin: "0px", ...style1, ...style2 }}>
+//         Hello World
+//       </h1>
+//     </>
+//   );
+// }
+
+// export default App;
