e0ab22cea743b3564746c1be69a99081e71626dd
M-Asimm <kpkprince55persia@gmail.com>
2023-10-29 11:01:04 +0500

    some ui is setting

--- passward-generator/src/components/Passward.jsx
+++ passward-generator/src/components/Passward.jsx
@@ -3,16 +3,36 @@ import React from "react";
 const Passward = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center my-[20px]">Passward Generator</h1>
-      <div className="flex justify-center">
-        <div className="flex bg-gray-300 rounded-full w-2/4 overflow-hidden">
-          <input
-            className="bg-blue-500 text-white px-5 border-none outline-none py-2 w-full placeholder-white"
-            type="text"
-            placeholder="Password"
-            readOnly
-          />
-          <label htmlFor="copy" className="py-2 px-5 cursor-pointer">Copy</label>
+      <h1 className="text-4xl font-bold text-center my-[20px]">
+        Passward Generator
+      </h1>
+      <div className="flex justify-center h-screen items-center">
+        <div className="bg-red-400 p-10 rounded-3xl">
+          <div className="flex bg-gray-300 rounded-full w-full overflow-hidden">
+            <input
+              className="bg-blue-500 text-white px-5 border-none outline-none py-2 w-full placeholder-white"
+              type="text"
+              placeholder="Password"
+              readOnly
+            />
+            <label htmlFor="copy" className="py-2 px-5 cursor-pointer">
+              Copy
+            </label>
+          </div>
+          <div className="mt-10 flex gap-5">
+            <div className="flex gap-2">
+              <input type="range" id="length" />
+              <label htmlFor="length">0 Length</label>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="char" />
+              <label htmlFor="char">Character</label>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="int" />
+              <label htmlFor="int">Integers</label>
+            </div>
+          </div>
         </div>
       </div>
     </div>
