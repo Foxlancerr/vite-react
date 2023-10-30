6d76b454c6367890670b283e96c024b9cd9c174a
M-Asimm <kpkprince55persia@gmail.com>
2023-10-30 11:19:23 +0500

    Half functionality is added

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,17 +1,53 @@
 import { useFetchCurrency } from "./hooks/index";
 import { InputBox } from "./components";
 import bgImage from "./assets/bgImage.jpg";
+import { useState } from "react";
 
 function App() {
-  console.log(useFetchCurrency("usd"));
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [cvtAmount, setCvtAmount] = useState(0);
+
+  // Custom Hooks is called
+  const currencyInfo = useFetchCurrency(from);
+  console.log(currencyInfo);
+
+  let currencyOptions = Object.keys(currencyInfo);
+  // console.log(currencyOptions);
+
+  // swap fuunction which will swap the curency to each other
+  const swap = () => {
+    setTo(from);
+    setFrom(to);
+    setCvtAmount(amount);
+    setAmount(cvtAmount);
+  };
+
+  const convert = () => {
+    setCvtAmount(amount * currencyInfo[to]);
+  };
+
+  const onAmountChange = (value) => {
+    console.log(typeof value);
+  };
+  const onCurrencyChange = (value) => {
+    console.log(typeof value);
+  };
   return (
     <div
       style={{ backgroundImage: `url(${bgImage})` }}
       className="h-screen w-full bg-cover bg-center bg-opacity-0 bg-no-repeat flex justify-center items-center text-white"
     >
       <div className="w-3/5 bg-white bg-opacity-40 p-10 rounded-[10px] gap-5 flex flex-col relative">
-        <InputBox />
-        <button className="bg-red-800 w-max px-[25px] py-[8px] rounded-[10px] font-bold absolute bottom-[50%] left-[50%] bg-opacity-70 hover:bg-opacity-100">
+        <InputBox
+          onCurrencyChange={onCurrencyChange}
+          // currencyOptions={currencyOptions}
+        />
+        <button
+          className="bg-red-800 w-max px-[25px] py-[8px] rounded-[10px] font-bold absolute bottom-[50%] left-[50%] bg-opacity-70 hover:bg-opacity-100"
+          onClick={swap}
+        >
           Swap
         </button>
         <InputBox />
--- currency-converter/src/components/InputBox.jsx
+++ currency-converter/src/components/InputBox.jsx
@@ -1,26 +1,43 @@
 import React from "react";
 
-const InputBox = () => {
+const InputBox = ({
+  label = "From",
+  amount,
+  onAmountChange,
+  currencyOptions = [],
+  onCurrencyChange,
+  selectCurrency = "usd",
+}) => {
   return (
     <div className="flex items-center justify-between bg-blue-800 p-4 rounded-[10px] gap-5">
       <div className="flex flex-col gap-2">
-        <label htmlFor="From" className="font-bold text-[20px]">From</label>
+        <label htmlFor="From" className="font-bold text-[20px]">
+          {label}
+        </label>
         <input
           className="border-none outline-none px-[10px] py-[5px] text-blue-800 rounded-[8px]"
           type="number"
           id="from"
-          value={0}
+          value={amount}
+          onChange={(e) => {
+            onAmountChange && onAmountChange(Number(e.target.value));
+          }}
         />
       </div>
       <div className="flex flex-col gap-2">
-        <p className="font-bold text-[18px]" >Currency Type</p>
+        <p className="font-bold text-[18px]">Currency Type</p>
         <select
-          name=""
-          id=""
           className="border-none outline-none px-[10px] py-[5px] text-blue-800 rounded-[8px]"
+          value={selectCurrency}
+          onChange={(e) => {
+            onCurrencyChange && onCurrencyChange(e.target.value);
+          }}
         >
-          <option value="usd">Usd</option>
-          <option value="usd">Inr</option>
+          {currencyOptions.map((cur) => (
+            <option key={cur} value={cur}>
+              {cur}
+            </option>
+          ))}
         </select>
       </div>
     </div>
