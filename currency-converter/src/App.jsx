f60ae9a2edfb4c04c4201e2106f0a3ac7147acec
M-Asimm <kpkprince55persia@gmail.com>
2023-10-30 12:39:27 +0500

    some bug is fixed

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -4,57 +4,78 @@ import bgImage from "./assets/bgImage.jpg";
 import { useState } from "react";
 
 function App() {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState(null);
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("inr");
-  const [cvtAmount, setCvtAmount] = useState(0);
+  const [cvtAmount, setCvtAmount] = useState(null);
 
   // Custom Hooks is called
   const currencyInfo = useFetchCurrency(from);
-  console.log(currencyInfo);
-
-  let currencyOptions = Object.keys(currencyInfo);
-  // console.log(currencyOptions);
+  const currencyOptions = Object.keys(currencyInfo);
 
   // swap fuunction which will swap the curency to each other
   const swap = () => {
-    setTo(from);
     setFrom(to);
-    setCvtAmount(amount);
+    setTo(from);
     setAmount(cvtAmount);
+    setCvtAmount(amount);
   };
 
   const convert = () => {
     setCvtAmount(amount * currencyInfo[to]);
   };
 
-  const onAmountChange = (value) => {
-    console.log(typeof value);
-  };
-  const onCurrencyChange = (value) => {
-    console.log(typeof value);
-  };
   return (
     <div
       style={{ backgroundImage: `url(${bgImage})` }}
       className="h-screen w-full bg-cover bg-center bg-opacity-0 bg-no-repeat flex justify-center items-center text-white"
     >
-      <div className="w-3/5 bg-white bg-opacity-40 p-10 rounded-[10px] gap-5 flex flex-col relative">
-        <InputBox
-          onCurrencyChange={onCurrencyChange}
-          // currencyOptions={currencyOptions}
-        />
+      <form
+        className="w-3/5 bg-white bg-opacity-40 p-10 rounded-[10px]"
+        onSubmit={(e) => {
+          e.preventDefault();
+          convert();
+        }}
+      >
+        <div className="flex flex-col relative gap-6">
+          <InputBox
+            label="From"
+            onCurrencyChange={(currency) => {
+              setFrom(currency);
+            }}
+            onAmountChange={(val) => {
+              setAmount(val);
+            }}
+            currencyOptions={currencyOptions}
+            amount={amount}
+            selectCurrency={from}
+          />
+          <button
+            className="bg-red-800 w-max px-[25px] py-[8px] rounded-[10px] font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-opacity-70 hover:bg-opacity-100"
+            onClick={swap}
+          >
+            Swap
+          </button>
+          <InputBox
+            label="To"
+            onCurrencyChange={(currency) => {
+              setTo(currency);
+            }}
+            onAmountChange={(value) => {
+              setAmount(value);
+            }}
+            currencyOptions={currencyOptions}
+            amount={cvtAmount}
+            selectCurrency={to}
+          />
+        </div>
         <button
-          className="bg-red-800 w-max px-[25px] py-[8px] rounded-[10px] font-bold absolute bottom-[50%] left-[50%] bg-opacity-70 hover:bg-opacity-100"
-          onClick={swap}
+          className="bg-red-800 font-bold px-[15px] py-[8px] rounded-[10px] bg-opacity-70 hover:bg-opacity-100"
+          type="submit"
         >
-          Swap
-        </button>
-        <InputBox />
-        <button className="bg-red-800 font-bold px-[15px] py-[8px] rounded-[10px] bg-opacity-70 hover:bg-opacity-100">
           Convert
         </button>
-      </div>
+      </form>
     </div>
   );
 }
