2b36719f8ab5e370271a67029a650ba223b99865
M-Asimm <kpkprince55persia@gmail.com>
2023-11-23 21:03:58 +0500

    dynamics routes completed

--- 09-react-router-method1/src/components/EachJeanDetails.jsx
+++ 09-react-router-method1/src/components/EachJeanDetails.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const EachJeanDetails = () => {
+  const { id } = useParams();
+
   return (
     <div className="flex justify-center items-center h-[50vh]">
       <h1 className=" text-slate-400 text-3xl font-bold">
-        This is a Jean Details Page which have the ID is: Id
+        This is a Jean Details Page which have the ID is: {id}
       </h1>
     </div>
   );
