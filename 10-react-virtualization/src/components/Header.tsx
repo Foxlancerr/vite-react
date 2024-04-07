234f5dd7e4afa9af6c7143a68e50ed4f44e49493
M-Asimm <kpkprince55persia@gmail.com>
2024-04-07 14:54:52 +0500

    React Performance Concept vartualization is completed for rending List and tables

---
+++ 10-react-virtualization/src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <nav>
+      <h1 className="flex justify-center items-center bg-slate-300 px-4 py-2 text-3xl font-bold ">Virtualization - Concepts</h1>
+    </nav>
+  );
+}
+
+export default Header;
